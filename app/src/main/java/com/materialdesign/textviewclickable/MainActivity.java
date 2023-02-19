3a5e18a74177320bc7c178d3a205df4053a8b262
Luis Fernando Machado <>
2023-02-18 23:43:29 -0300

    Event of ClickableSpan

--- app/src/main/java/com/materialdesign/textviewclickable/MainActivity.java
+++ app/src/main/java/com/materialdesign/textviewclickable/MainActivity.java
@@ -1,13 +1,46 @@
 package com.materialdesign.textviewclickable;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView textView = findViewById(R.id.text_view);
+		String text = "I wnat THIS and THIS to be clickable";
+		SpannableString ss = new SpannableString(text);
+
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+			@Override
+			public void onClick(@NonNull View view) {
+                Toast.makeText(MainActivity.this, "Section One, Checked", Toast.LENGTH_SHORT).show();
+			}
+		};
+
+        ClickableSpan clickableSpan2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Toast.makeText(MainActivity.this, "Section Two, checked", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        ss.setSpan(clickableSpan1, 7, 11, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(clickableSpan2, 16, 20, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+	}
 }
